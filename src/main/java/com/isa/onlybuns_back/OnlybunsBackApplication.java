70a39b07272f93f94b6167eb3d87230edb64afa8
stasaaa <radojicic.stasa@gmail.com>
2024-11-05 12:58:14 +0100

    Initial commit

---
+++ src/main/java/com/isa/onlybuns_back/OnlybunsBackApplication.java
@@ -0,0 +1,13 @@
+package com.isa.onlybuns_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlybunsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlybunsBackApplication.class, args);
+	}
+
+}
