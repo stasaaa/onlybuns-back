caf4527b3c8694f9c76ba2af96a47b4c284b0600
stasaaa <radojicic.stasa@gmail.com>
2025-09-10 16:54:48 +0200

    fix develop branch

--- only-buns-back/src/main/java/com/isa/onlybuns_back/repository/RabbitCareLocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isa.onlybuns_back.repository;
-
-import com.isa.onlybuns_back.model.RabbitCareLocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RabbitCareLocationRepository extends JpaRepository<RabbitCareLocation, Long> {
-}
