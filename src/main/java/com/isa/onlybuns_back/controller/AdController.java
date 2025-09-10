284d116401b58539834b6c6fc465802a9e28a831
Milica Vujkov <milavujkov@gmail.com>
2025-09-10 17:00:54 +0200

    notification statistics returned to true files

--- src/main/java/com/isa/onlybuns_back/controller/AdController.java
+++ src/main/java/com/isa/onlybuns_back/controller/AdController.java
@@ -32,6 +32,6 @@ public class AdController {
         }
 
         adService.sendAdMessage(post);
-        return ResponseEntity.ok("Objava #" + postId + " odobrena za reklamiranje.");
+        return ResponseEntity.ok("Post suggested for advertising!");
     }
 }
