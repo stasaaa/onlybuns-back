d3e2f484646c6e5503cf2fc25fa1692339f63841
stasaaa <radojicic.stasa@gmail.com>
2025-09-10 21:36:09 +0200

    final check

--- src/main/java/com/isa/onlybuns_back/controller/RabbitCareLocationController.java
+++ src/main/java/com/isa/onlybuns_back/controller/RabbitCareLocationController.java
@@ -4,6 +4,7 @@ import com.isa.onlybuns_back.model.RabbitCareLocation;
 import com.isa.onlybuns_back.service.RabbitLocationConsumerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class RabbitCareLocationController {
     private RabbitLocationConsumerService service;
 
     @GetMapping("")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<RabbitCareLocation>> getAllLocations() {
         return ResponseEntity.ok().body(service.getAllLocations());
     }
